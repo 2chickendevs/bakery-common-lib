cf4c37572fd5b80f3e88ec7d3cad787e7b7f77e2
nntan041299 <147411974+nntan041299@users.noreply.github.com>
2025-12-15 22:28:44 +0700

    Set up the pom file to use parent lib, add exception and filter classes (#1) (#3)

--- src/main/java/com/twochickendevs/bakerycommonlib/BakeryCommonLibApplication.java
+++ src/main/java/com/twochickendevs/bakerycommonlib/BakeryCommonLibApplication.java
@@ -1,13 +1,7 @@
 package com.twochickendevs.bakerycommonlib;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BakeryCommonLibApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BakeryCommonLibApplication.class, args);
-	}
+	public static void main(String[] args) {}
 
 }
