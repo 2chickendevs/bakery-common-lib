66e26df080ee76c29bb3781efc42dd83ac4cabd1
annguyen <nntan041299.app@gmail.com>
2025-12-22 23:12:49 +0700

    Fix issue always return 500 error code (#11)

--- src/main/java/com/twochickendevs/bakerycommonlib/exception/advide/ExceptionAdvice.java
+++ src/main/java/com/twochickendevs/bakerycommonlib/exception/advide/ExceptionAdvice.java
@@ -1,6 +1,6 @@
 package com.twochickendevs.bakerycommonlib.exception.advide;
 
-import com.twochickendevs.bakerycommonlib.exception.ExistingException;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class RunTimeExceptionAdvice {
+@Order(999)
+public class ExceptionAdvice {
 
     @ResponseBody
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String existing(RuntimeException ex) {
+    public String existing(Exception ex) {
         return ex.getMessage();
     }
 }
